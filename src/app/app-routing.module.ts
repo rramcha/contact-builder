cbba3773f10355e9e88021cd38dcb6bf4cf3a51f
Ravi <ravindrar398@gmail.com>
2018-08-13 22:13:31 -0500

    router handling for user listing & posts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {HomeComponent} from './components/home/home.component';
+import {UsersComponent} from './components/users/users.component';
+import {PostsComponent} from './components/posts/posts.component';
+
+var routes : Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'users', component: UsersComponent },
+  {path: 'posts', component: PostsComponent }
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { UserService } from './services/user.service';
 import { PostsService } from './services/posts.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 
 
@@ -24,12 +26,14 @@ import { PostFormComponent } from './components/post-form/post-form.component';
     UsersComponent,
     NavbarComponent,
     PostsComponent,
-    PostFormComponent
+    PostFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [UserService, PostsService],
   bootstrap: [AppComponent]
